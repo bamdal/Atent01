8f0dd877c429145e84e59c85be5f047a339d269d
HAENAA <tlsthud@naver.com>
2024-01-10 14:30:59 +0900

    싱글톤게임매니저

---
+++ Unity/23.12/02_Shooting/Assets/_Script/Test/Test_Singleton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_Singleton : TestBase
+{
+    protected override void OnTest1(InputAction.CallbackContext context)
+    {
+        //Singleton s = Singleton.Instance;
+
+
+    }
+
+
+
+}
+
+
